9c551d536cf2b577ab384083c3545ba8784c7a12
mohsinehour <hourmatallahmohsine@gmail.com>
2024-07-26 00:16:56 +0100

    task

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -11,10 +11,10 @@ def pascal_triangle(n):
   res = []
   if n > 0:
     for i in range(1, n + 1):
-        level = []
-        C = 1
-        for j in range(1, i + 1):
-            level.append(C)
-            C = C * (i - j) // j
-        res.append(level)
+      level = []
+      C = 1
+      for j in range(1, i + 1):
+        level.append(C)
+        C = C * (i - j) // j
+      res.append(level)
   return res
